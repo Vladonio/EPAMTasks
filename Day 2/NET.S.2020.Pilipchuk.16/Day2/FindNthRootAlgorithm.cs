16ccf2782ca766337125d96cdab18e4d22ae5a9f
Vladonio <47864655+Vladonio@users.noreply.github.com>
2020-03-17 20:00:50 +0300

    5 tasks completed

---
+++ Day 2/NET.S.2020.Pilipchuk.16/Day2/FindNthRootAlgorithm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class FindNthRootAlgorithm
+    {
+
+        //Xk+1 = Xk * (1 - (1 - (A / Xk^n)) / n);
+        public double FindNthRoot(double x, double n, double e)
+        {
+            var x0 = x / n; //начальное предположение
+            var x1 = x0 * (1 - (1 - (x / Math.Pow(x0, n))) / n);
+            while (Math.Abs(x1 - x0) > e)
+            {
+                x0 = x1;
+                x1 = x0 * (1 - (1 - (x / Math.Pow(x0, n))) / n);
+            }
+            return Math.Round(x1, 3);
+        }
+    }
+}
---
+++ Day 2/NET.S.2020.Pilipchuk.16/Day2/NextBiggerNumberAlgorithm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class NextBiggerNumberAlgorithm
+    {
+        private HashSet<int> possibleNumbers;
+
+        private void SetAllPossibleNumbers(List<int> digits, string currNumber = "")
+        {
+            if (digits.Count == 0)
+            {
+                possibleNumbers.Add(int.Parse(currNumber));
+                return;
+            }
+
+            for (int i = 0; i < digits.Count; i++)
+            {
+                List<int> newDigits = new List<int>(digits);
+                newDigits.RemoveAt(i);
+                SetAllPossibleNumbers(newDigits, currNumber + digits[i].ToString());
+            }
+        }
+
+        public int FindNextBiggerNumber(int number)
+        {
+            possibleNumbers = new HashSet<int>();
+            SetAllPossibleNumbers(GetDigitsFromNumber(number));
+
+            int min = int.MaxValue;
+            foreach (int item in possibleNumbers)
+            {
+                if (item > number && item < min)
+                {
+                    min = item;
+                }
+            }
+
+            Console.WriteLine(DateTime.Now);
+            if (min == int.MaxValue)
+                return -1;
+            return min;
+        }
+
+        private List<int> GetDigitsFromNumber(int number)
+        {
+            List<int> digits = new List<int>();
+            int digitToAdd;
+            while (number > 0)
+            {
+                digitToAdd = number % 10;
+                number /= 10;
+                digits.Insert(0, digitToAdd);
+            }
+            return digits;
+        }
+    }
+}
