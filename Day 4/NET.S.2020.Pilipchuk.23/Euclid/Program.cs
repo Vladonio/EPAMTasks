9aae0e3db96f1b4d91d8c8d967e9272cec1ce008
Vladonio <47864655+Vladonio@users.noreply.github.com>
2020-03-23 22:26:28 +0300

    Tasks + unit tests completed

---
+++ Day 4/NET.S.2020.Pilipchuk.23/Euclid/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euclid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch elapsedTicks = new Stopwatch();
+            double kek = 255.255;
+            Console.WriteLine(kek.GetDoubleInBinary());
+
+            EuclidAlgorithms alg = new EuclidAlgorithms();
+            Console.WriteLine(alg.EuclidGCD(ref elapsedTicks, 54, 36, 12));
+            Console.WriteLine(elapsedTicks.ElapsedTicks);
+            
+        }
+    }
+}
